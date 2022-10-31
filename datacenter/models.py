f0b1529c246452e679d7ddbbda420aff703c60af
s-kella <52741545+s-kella@users.noreply.github.com>
2022-10-31 16:35:00 -0500

    Add files via upload

---
+++ datacenter/models.py
@@ -0,0 +1,43 @@
+import datetime
+from django.db import models
+from django.utils import timezone
+
+
+class Passcard(models.Model):
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now=True)
+    passcode = models.CharField(max_length=200, unique=True)
+    owner_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        if self.is_active:
+            return self.owner_name
+        return f'{self.owner_name} (inactive)'
+
+
+class Visit(models.Model):
+    created_at = models.DateTimeField(auto_now=True)
+    passcard = models.ForeignKey(Passcard, on_delete=models.CASCADE)
+    entered_at = models.DateTimeField()
+    leaved_at = models.DateTimeField(null=True)
+
+    def get_duration(self):
+        return timezone.now() - timezone.localtime(self.entered_at) \
+            if self.leaved_at is None \
+            else self.leaved_at - self.entered_at
+
+    def is_visit_long(self, minutes=60):
+        if self.get_duration() > datetime.timedelta(minutes=minutes):
+            return True
+        else:
+            return False
+
+    def __str__(self):
+        return '{user} entered at {entered} {leaved}'.format(
+            user=self.passcard.owner_name,
+            entered=self.entered_at,
+            leaved=(
+                f'leaved at {self.leaved_at}'
+                if self.leaved_at else 'not leaved'
+            )
+        )
