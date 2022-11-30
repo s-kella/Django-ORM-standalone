f98eb4960e306174a6b05bad30763979d58a9f03
s-kella <52741545+s-kella@users.noreply.github.com>
2022-11-30 12:57:33 -0500

    add default values

--- project/settings.py
+++ project/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 from environs import Env
 
 
@@ -17,13 +18,13 @@ DATABASES = {
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY', 'REPLACE_ME')
 
-DEBUG = env.bool('DEBUG')
+DEBUG = env.bool('DEBUG', False)
 
 ROOT_URLCONF = 'project.urls'
 
-ALLOWED_HOSTS = env('ALLOWED_HOSTS')
+ALLOWED_HOSTS = env('ALLOWED_HOSTS', ['*'])
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
