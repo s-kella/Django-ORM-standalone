5d5e4e3602b16e1532347644ff57dbad8711e77e
s-kella <52741545+s-kella@users.noreply.github.com>
2022-12-01 08:05:36 -0500

    change ALLOWED_HOSTS default value

--- project/settings.py
+++ project/settings.py
@@ -16,7 +16,7 @@ DEBUG = env.bool('DEBUG', False)
 
 ROOT_URLCONF = 'project.urls'
 
-ALLOWED_HOSTS = env('ALLOWED_HOSTS', ['*'])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['localhost', '127.0.0.1:8000'])
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
